8aa50f3e73877799d08ca46162ba131a481824fc
Villatelier <124809163+Villatelier@users.noreply.github.com>
2025-11-04 00:20:35 +0000

    VirtualTour360
    Upload inicial do projeto VirtualTour360

---
+++ app-files/data.js
@@ -0,0 +1,323 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cam010_post",
+      "name": "CAM010_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.1965395094419904,
+        "pitch": -0.006073978442710626,
+        "fov": 1.0841904824255306
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7862356986489925,
+          "pitch": 0.3745713719341417,
+          "rotation": 0,
+          "target": "1-cam011_post"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cam011_post",
+      "name": "CAM011_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09363569619732992,
+          "pitch": 0.29087950016616304,
+          "rotation": 0,
+          "target": "2-cam012_post"
+        },
+        {
+          "yaw": 0.3274356203193278,
+          "pitch": 0.1780707399971515,
+          "rotation": 0,
+          "target": "3-cam013_post"
+        },
+        {
+          "yaw": -2.223007913749406,
+          "pitch": 0.5824715187231906,
+          "rotation": 0,
+          "target": "0-cam010_post"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-cam012_post",
+      "name": "CAM012_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4327639667774861,
+          "pitch": 0.30410327302587703,
+          "rotation": 0,
+          "target": "3-cam013_post"
+        },
+        {
+          "yaw": 0.8020139806592681,
+          "pitch": 0.13759219630996,
+          "rotation": 0,
+          "target": "4-cam014_post"
+        },
+        {
+          "yaw": 1.4577979552317997,
+          "pitch": 0.2606793520699746,
+          "rotation": 0,
+          "target": "5-cam015_post"
+        },
+        {
+          "yaw": -2.7460306024082612,
+          "pitch": 0.29040709656103125,
+          "rotation": 0,
+          "target": "0-cam010_post"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-cam013_post",
+      "name": "CAM013_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.002249417990716651,
+          "pitch": 0.28241804318375685,
+          "rotation": 0,
+          "target": "4-cam014_post"
+        },
+        {
+          "yaw": 0.7492172076736736,
+          "pitch": 0.2661250578607479,
+          "rotation": 0,
+          "target": "5-cam015_post"
+        },
+        {
+          "yaw": 2.518604287239665,
+          "pitch": 0.4935239494955965,
+          "rotation": 0,
+          "target": "2-cam012_post"
+        },
+        {
+          "yaw": 2.0587408028679395,
+          "pitch": 0.3083637593452657,
+          "rotation": 0,
+          "target": "1-cam011_post"
+        },
+        {
+          "yaw": 2.1102100762992873,
+          "pitch": 0.15384779887618727,
+          "rotation": 0,
+          "target": "0-cam010_post"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-cam014_post",
+      "name": "CAM014_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2906974389569008,
+          "pitch": 0.36722572360691963,
+          "rotation": 0,
+          "target": "5-cam015_post"
+        },
+        {
+          "yaw": 0.5290383101623544,
+          "pitch": 0.17202520494161355,
+          "rotation": 0,
+          "target": "1-cam011_post"
+        },
+        {
+          "yaw": 0.9113152761280201,
+          "pitch": 0.22899116083303817,
+          "rotation": 0,
+          "target": "2-cam012_post"
+        },
+        {
+          "yaw": 1.4332750556707916,
+          "pitch": 0.41354794206780277,
+          "rotation": 0,
+          "target": "3-cam013_post"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-cam015_post",
+      "name": "CAM015_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3309897658607284,
+          "pitch": 0.2974731321970179,
+          "rotation": 0,
+          "target": "4-cam014_post"
+        },
+        {
+          "yaw": 0.7016998288824787,
+          "pitch": 0.16478454618261829,
+          "rotation": 0,
+          "target": "3-cam013_post"
+        },
+        {
+          "yaw": 0.06042759950637411,
+          "pitch": 0.2389213839186901,
+          "rotation": 0,
+          "target": "2-cam012_post"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "VirtualTour360",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
